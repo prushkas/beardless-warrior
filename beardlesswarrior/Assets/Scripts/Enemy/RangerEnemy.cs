e4652ee2d3a577f989bd863b2cb08f36e7a93f39
Migalhaz <105467709+Migalhaz@users.noreply.github.com>
2023-07-28 23:30:31 -0300

    Inimigo que atira coisas
    hahaha ele atira loucamente uou

--- beardlesswarrior/Assets/Scripts/Enemy/RangerEnemy.cs
+++ beardlesswarrior/Assets/Scripts/Enemy/RangerEnemy.cs
@@ -2,25 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RangerEnemy : MonoBehaviour
+public class RangerEnemy : AbstractEnemy
 {
-    [Header("Range Settings")]
+    [Header("Ranger Settings")]
     [SerializeField] Range m_rangeTimeToAttack;
     float m_currentTimeToAttack;
-
-    private void Awake()
+    [SerializeField] GameObject m_enemyBulletPrefab;
+    protected override void Start()
     {
+        base.Start();
         m_currentTimeToAttack = m_rangeTimeToAttack.GetRandomValue();
     }
-
     private void Update()
     {
-
+        AttackTimer();
     }
-
+    void AttackTimer()
+    {
+        if (m_currentTimeToAttack <= m_rangeTimeToAttack.m_MinValue)
+        {
+            Attack();
+        }
+        m_currentTimeToAttack -= Time.deltaTime;
+    }
     void Attack()
     {
         m_currentTimeToAttack = m_rangeTimeToAttack.GetRandomValue();
-
+        Instantiate(m_enemyBulletPrefab, transform.position, Quaternion.Euler(0f, 0f, AimAngle()));
+    }
+    float AimAngle()
+    {
+        Vector3 aimDirection = m_playerTransform.position - transform.position;
+        return Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
     }
 }
