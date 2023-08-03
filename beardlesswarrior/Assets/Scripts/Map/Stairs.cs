3d569c5ea4654a2eec3a9c9a0e6d545b8e2796d0
Migalhaz <105467709+Migalhaz@users.noreply.github.com>
2023-08-03 13:04:07 -0300

    revisao e atualizacao do mapa

--- beardlesswarrior/Assets/Scripts/Map/Stairs.cs
+++ beardlesswarrior/Assets/Scripts/Map/Stairs.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class Stairs : MonoBehaviour
 {
+    [SerializeField] bool m_lastFloor;
     [SerializeField] Transform m_spawnPosition;
     [SerializeField] MapCameraUpdate m_newRoom;
     public void NextLevel()
     {
+        if (m_lastFloor) 
+        { 
+            GameManager.Instance.GameOver(true);
+            return;
+        }
         PlayerManager.Instance.transform.position = m_spawnPosition.position;
         m_newRoom.TPCam();
     }
