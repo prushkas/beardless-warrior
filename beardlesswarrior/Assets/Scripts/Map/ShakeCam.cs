8383362885f22f7469116fe274dbe6cbc5a8e911
Migalhaz <miguel.chagas614@gmail.com>
2023-08-04 20:55:04 -0300

    update no shakecam uou nao foi o ultimo commit mas esse acredito ser

--- beardlesswarrior/Assets/Scripts/Map/ShakeCam.cs
+++ beardlesswarrior/Assets/Scripts/Map/ShakeCam.cs
@@ -17,7 +17,7 @@ public class ShakeCam : Singleton<ShakeCam>
         Vector3 newPosition = new();
         while (elapsed < duration)
         {
-            if (GameManager.Instance.m_Paused) break;
+            if (BreakShakeEffect()) break;
             float x = Random.Range(-1f, 1f) * magnitude;
             float y = Random.Range(-1f, 1f) * magnitude;
             newPosition.Set(transform.position.x + x, transform.position.y + y, -10f);
@@ -26,5 +26,10 @@ public class ShakeCam : Singleton<ShakeCam>
             yield return 0;
         }
         transform.position = m_orignalPosition;
+    }
+
+    bool BreakShakeEffect()
+    {
+        return GameManager.Instance.m_Paused || GameManager.Instance.m_GameOver;
     }
 }
